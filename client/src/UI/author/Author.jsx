5ada89c241a9070448501cafd4e1c97aa8de14c2
mrKlava <mr.a.klava@gmail.com>
2024-01-10 14:04:00 +0100

    improved styles and form validation

--- client/src/UI/author/Author.jsx
+++ client/src/UI/author/Author.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Avatar from '../avatar/Avatar'
+import { Link } from "react-router-dom"
+import Avatar from "../avatar/Avatar"
+import moment from "moment"
 
-import './author.scss'
-import moment from 'moment'
+import "./author.scss"
 
-function Author({ author, className='', small=false, location=true, date=true }) {
-  const classes = className ? 'author ' + className : 'author'
+function Author({ author, className="", small=false, location=true, date=true }) {
+  const classes = className ? "author " + className : "author"
 
   return (
     <div className={classes}>
-      <Link className='author-link' to={`/profile/${author.user_id}`} >
-        <Avatar img={author.user_img} small={small} />
+      <Link className="author-link" to={`/profile/${author.user_id}`} >
+        <Avatar img={author.user_img} alt={`avatar of ${author.firstname} ${author.lastname}`} small={small} />
         <div className="author-stamp">
-          <span className='author-stamp_name'>{author.firstname} {author.lastname}</span>
-          { date && author.created && <span className='author-stamp_date'>{moment(author.created).fromNow()} </span> }
-          { location && author.location && <span className='author-stamp_location'>{author.location} </span> }
+          <span className="author-stamp_name">{author.firstname} {author.lastname}</span>
+          { date && author.created && <span className="author-stamp_date">{moment(author.created).fromNow()} </span> }
+          { location && author.location && <span className="author-stamp_location">{author.location} </span> }
         </div>
       </Link>
     </div>
